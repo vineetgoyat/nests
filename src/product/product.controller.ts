d817d45da059b87deb63524487a0d7435a355449
Vineet <vineetgoyat1@gmail.com>
2026-07-10 17:52:24 +0530

    lrning....

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private productService: ProductService) {}
+    @Get()
+    getProducts() {
+        return this.productService.getAllProducts();
+}
+@Get(':id')
+getProductById(@Param('id') id: string) {
+    return this.productService.getProductById(Number(id));
+}
+}
