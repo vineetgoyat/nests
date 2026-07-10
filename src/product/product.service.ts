2f21ddc435f1c5aee560189c331e5e3a77dbe6d0
Vineet <vineetgoyat1@gmail.com>
2026-07-10 16:22:24 +0530

    lrning....

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    private products = [
+        {id:1, name:'kachua', price:10000},
+        {id:2, name:'bandar', price:10000},
+        {id:3, name:'genda', price:10000}
+
+    ]
+}
