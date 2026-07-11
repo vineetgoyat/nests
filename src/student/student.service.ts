4ffa42db02c4ff8fb1e6bfc9e5439ac6dca7db22
Vineet <vineetgoyat1@gmail.com>
2026-07-11 16:26:23 +0530

    lrning....

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    private students =[
+        {id: 1,name:'Gajju', age: 2},
+        {id: 2,name:'Chajju', age: 4},
+    ];
+    getAllStudents(){
+        return this.students;
+    }
+    getStudentsById(id:number){
+        const student = this.students.find((s) => s.id === id);
+        if(!student){
+            throw new NotFoundException('Student not found');
+        }
+        return student;
+    }
+    //post method
+    createStudent(data:{name:string; age:number}){
+        const newStudent ={
+            id: Date.now(),
+            ...data,
+        };
+        this.students.push(newStudent);
+        return newStudent;
+    }
+
+
+}
