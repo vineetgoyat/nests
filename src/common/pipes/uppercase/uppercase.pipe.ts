940dba9c1f2c65f010ace3b660c1c41fc8198419
Vineet <vineetgoyat1@gmail.com>
2026-07-11 22:19:01 +0530

    lrning....

--- src/common/pipes/uppercase/uppercase.pipe.ts
+++ src/common/pipes/uppercase/uppercase.pipe.ts
@@ -3,6 +3,9 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class UppercasePipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
+    if(typeof value === 'string'){
+      return value.toUpperCase();
+    }
     return value;
   }
 }
