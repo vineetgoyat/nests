60233a56bb6c0f4be2b94a17b152c84df3ca6337
Vineet <vineetgoyat1@gmail.com>
2026-07-11 18:09:07 +0530

    lrning....

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { StudentService } from './student.service';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor (private readonly StudentService: StudentService){}
+    
+
+}
