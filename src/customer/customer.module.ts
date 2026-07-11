3b1772428e4c8d7c3f33e238061be49e40ac094b
Vineet <vineetgoyat1@gmail.com>
2026-07-11 19:19:10 +0530

    lrning....

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CustomerController } from './customer.controller';
 
-@Module({})
+@Module({
+  controllers: [CustomerController]
+})
 export class CustomerModule {}
