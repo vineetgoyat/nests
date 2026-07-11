9f5c2df775f733fc89259d24f7098a78b11daf67
Vineet <vineetgoyat1@gmail.com>
2026-07-11 15:58:44 +0530

    lrning....

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { EmployeeModule } from './employee/employee.module';
 import { CategoryModule } from './category/category.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [EmployeeModule, CategoryModule],
+  imports: [EmployeeModule, CategoryModule, StudentModule],
   controllers: [AppController, UserController, ProductController],
   providers: [AppService, ProductService],
 })
