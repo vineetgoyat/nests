8891f2b6dd56ecec0ce185774451de15b075831e
Vineet <vineetgoyat1@gmail.com>
2026-07-11 19:55:19 +0530

    lrning....

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Customer } from './interfaces/customer.interface';
+import { createCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
 export class CustomerService {
@@ -8,5 +9,14 @@ export class CustomerService {
     getAllcustomers() : Customer[]{
         return this.customers;
 
+    }
+    addCustomer( createCustomerDto : createCustomerDto) : Customer{
+        const newCustomer : Customer = {
+            id:Date. now(),
+            ...createCustomerDto,
+        };
+        this.customers.push(newCustomer);
+        return newCustomer;
+    
     }
 }
