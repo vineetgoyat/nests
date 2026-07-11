635ee5f57503220d7f65277f37dfdb3189f1d5b2
Vineet <vineetgoyat1@gmail.com>
2026-07-11 21:26:18 +0530

    lrning....

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { createCustomerDto } from './dto/create-customer.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+    constructor (private readonly customerservice: CustomerService){}
+    @Get()
+    getCustomers(){
+        return this.customerservice.getAllcustomers();
+    }
+    @Post()
+    addCustomer(@Body() createCustomerDto : createCustomerDto){
+        return this.customerservice.addCustomer(createCustomerDto);
+    }
+
+
+}
