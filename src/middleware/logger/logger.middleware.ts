3d7479934e91c7ba92f89e732ab2b1dc4049ead1
Vineet <vineetgoyat1@gmail.com>
2026-07-12 20:09:17 +0530

    lrning....

--- src/middleware/logger/logger.middleware.ts
+++ src/middleware/logger/logger.middleware.ts
@@ -2,7 +2,8 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => NextFunction) {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(`[${req.method}]-[${req.url}]`)
     next();
   }
 }
