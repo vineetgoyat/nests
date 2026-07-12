855a88d5d4dc26d0bfac31d8c9623add26ed613a
Vineet <vineetgoyat1@gmail.com>
2026-07-12 17:15:16 +0530

    lrning....

--- src/guards/roles/roles.guard.ts
+++ src/guards/roles/roles.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -8,6 +9,15 @@ export class RolesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+      ROLES_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]
+    )
+    if(!requiredRoles) return true;
+    const request = context.switchToHttp().getRequest<{ headers : Record< string, string >}>();
+    const 
     return true;
   }
 }
