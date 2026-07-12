2e0e0bd677c9098ea5de9a6f905d14b7a06913ac
Vineet <vineetgoyat1@gmail.com>
2026-07-12 17:48:10 +0530

    lrning....

--- src/filters/http-exception/http-exception.filter.ts
+++ src/filters/http-exception/http-exception.filter.ts
@@ -2,5 +2,9 @@ import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 
 @Catch()
 export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+  }
 }
