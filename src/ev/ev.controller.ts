4ae331d5e7b858832df74897d4e9896e4d8d42c6
Vineet <vineetgoyat1@gmail.com>
2026-07-13 03:37:14 +0530

    lrning....

--- src/ev/ev.controller.ts
+++ src/ev/ev.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from './ev.service';
 
 @Controller('ev')
-export class EvController {}
+export class EvController {
+    constructor(private readonly configService: ConfigService){}
+
+    @Get()
+    getUrl(){
+        return this.configService.getDbUrl();
+    }
+}
