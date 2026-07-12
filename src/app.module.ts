2ce8a2a22ac634a518329e78232a644fa00511ec
Vineet <vineetgoyat1@gmail.com>
2026-07-12 17:21:29 +0530

    lrning....

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,11 @@ import { CategoryModule } from './category/category.module';
 import { StudentModule } from './student/student.module';
 import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
+import { UserRolesController } from './user-roles/user-roles.controller';
 
 @Module({
   imports: [EmployeeModule, CategoryModule, StudentModule, CustomerModule],
-  controllers: [AppController, UserController, ProductController, MynameController],
+  controllers: [AppController, UserController, ProductController, MynameController, UserRolesController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
