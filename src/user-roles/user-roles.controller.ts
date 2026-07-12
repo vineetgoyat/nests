ff8f3de171e5967192dc8faa1b00b3cd14a53ef8
Vineet <vineetgoyat1@gmail.com>
2026-07-12 17:33:02 +0530

    lrning....

--- src/user-roles/user-roles.controller.ts
+++ src/user-roles/user-roles.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/guards/roles/roles.decorator';
+import { Role } from 'src/guards/roles/roles.enums';
+import { RolesGuard } from 'src/guards/roles/roles.guard';
 
 @Controller('user-roles')
-export class UserRolesController {}
+export class UserRolesController {
+    @Get('admin-data')
+    @UseGuards(RolesGuard)
+    @Roles(Role.Admin)
+    getAdminData(){
+        return {messsage: 'only Admin can access'}
+    }
+    @Get('user-data')
+    getUserData(){
+        return {message: 'anyone can access'}
+    }
+}
