6f99dcce520ac4aeb9aba9cffc945be1c8dabd22
Vineet <vineetgoyat1@gmail.com>
2026-07-13 17:04:46 +0530

    lrning....

--- src/student1/student1.schema.ts
+++ src/student1/student1.schema.ts
@@ -1,6 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { timeStamp } from 'console';
 import { Document } from 'mongoose';
 
 export type Student1Document = Student1 & Document;
 
-@Schema
+@Schema({timestamps : true})
+export class Student1 {
+
+    @Prop({required : true})
+    name: string;
+
+    @Prop({required : true})
+    age: number;
+
+    @Prop()
+    email?: string;
+}
+
+export const Student1Schema = SchemaFactory.createForClass(Student1);
