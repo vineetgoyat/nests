fbd1b22f835ed47be9318f3a640b9cad0b4e55be
Vineet <vineetgoyat1@gmail.com>
2026-07-13 18:27:47 +0530

    lrning....

--- src/student1/student1.service.ts
+++ src/student1/student1.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Student1, Student1Document } from './student1.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class Student1Service {}
+export class Student1Service {
+    constructor(
+        @InjectModel(Student1.name) private student1Model: Model<Student1Document>
+
+    ){}
+
+    async createStudent1 (data: Partial<Student1>):Promise<Student1>{
+        const newStudent1 = new this.student1Model(data);
+        return newStudent1.save();
+    }
+}
