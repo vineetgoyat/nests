72862b63368048f08d9d5621c263aef5481eddee
Vineet <vineetgoyat1@gmail.com>
2026-07-13 18:49:02 +0530

    lrning....

--- src/student1/student1.controller.ts
+++ src/student1/student1.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Student1Service } from './student1.service';
 import { Student1 } from './student1.schema';
 
@@ -9,5 +9,10 @@ export class Student1Controller {
     @Post()
     async addStudent1(@Body() data: Partial<Student1>){
         return this.student1Service.createStudent1(data);
+    }
+
+    @Get()
+    async getStudent1(){
+        return this.student1Service.getAllStudents1();
     }
 }
--- src/student1/student1.service.ts
+++ src/student1/student1.service.ts
@@ -18,4 +18,8 @@ export class Student1Service {
     async getAllStudents1() : Promise<Student1[]>{
         return this.student1Model.find().exec();
     }
+
+    async getStudent1ById(id : string): Promise<Student1 | null>{
+        return this.student1Model.findById(id).exec();
+    }
 }
