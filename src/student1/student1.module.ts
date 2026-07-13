962f0a80553e3d51a5ee20403ed1d828cf8d5477
Vineet <vineetgoyat1@gmail.com>
2026-07-13 17:12:31 +0530

    lrning....

--- src/student1/student1.module.ts
+++ src/student1/student1.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Student1Schema } from './student1.schema';
 
-@Module({})
+@Module({
+    imports :[
+        MongooseModule.forFeature([{name : Student1Schema, schema : Student1Schema}])
+    ]})
+
+
 export class Student1Module {}
