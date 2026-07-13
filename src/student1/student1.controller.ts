469a155bc3072d3ad11b3bb556563a0a3880040f
Vineet <vineetgoyat1@gmail.com>
2026-07-13 18:31:59 +0530

    lrning....

--- src/student1/student1.controller.ts
+++ src/student1/student1.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Student1Service } from './student1.service';
+import { Student1 } from './student1.schema';
 
 @Controller('student1')
-export class Student1Controller {}
+export class Student1Controller {
+    constructor(private readonly student1Service: Student1Service){}
+
+    @Post()
+    async addStudent1(@Body() data: Partial<Student1>){
+        return this.student1Service.createStudent1(data);
+    }
+}
