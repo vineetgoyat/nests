3bb6cd07e5bfb331b7f548959c89027e2bc0922c
Vineet <vineetgoyat1@gmail.com>
2026-07-25 18:53:10 +0530

    continue

--- src/library/library.module.ts
+++ src/library/library.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LibraryService } from './library.service';
 import { LibraryController } from './library.controller';
+import { BookSchema } from './schemas/book.schema';
+import { LibrarySchema } from './schemas/library.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    {name: 'Library', schema: LibrarySchema},
+    {name: 'Book', schema: BookSchema},
+
+  ])],
   providers: [LibraryService],
   controllers: [LibraryController]
 })
