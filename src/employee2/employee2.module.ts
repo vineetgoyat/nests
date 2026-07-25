b94aeb15e16616232cddf97942a4990450d50cd7
Vineet <vineetgoyat1@gmail.com>
2026-07-25 15:59:14 +0530

    continue

--- src/employee2/employee2.module.ts
+++ src/employee2/employee2.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { Employee2Service } from './employee2.service';
 import { Employee2Controller } from './employee2.controller';
+import { Profile, ProfileSchema } from './schemas/profile.schema';
+import { Employee2, Employee2Schema } from './schemas/employee2.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: Employee2.name, schema: Employee2Schema},
+      {name: Profile.name, schema: ProfileSchema}
+    ])
+  ],
   providers: [Employee2Service],
   controllers: [Employee2Controller]
 })
