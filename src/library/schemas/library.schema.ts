e1c74752777aeb8377dbd91572b95036382e68c0
Vineet <vineetgoyat1@gmail.com>
2026-07-25 18:48:03 +0530

    continue

--- src/library/schemas/library.schema.ts
+++ src/library/schemas/library.schema.ts
@@ -1,2 +1,10 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Library extends Document {
+    @Prop()
+    'name': string;
+
+    @Prop({type: [{ type: Types.ObjectId, ref: 'Book'}]})
+}
