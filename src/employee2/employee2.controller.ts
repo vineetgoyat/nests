415484171369f1b4cff677eb95cac60c1566ad7c
Vineet <vineetgoyat1@gmail.com>
2026-07-25 16:36:39 +0530

    continue

--- src/employee2/employee2.controller.ts
+++ src/employee2/employee2.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { Employee2Service } from './employee2.service';
 
 @Controller('employee2')
-export class Employee2Controller {}
+export class Employee2Controller {
+    constructor(private readonly employee2Service: Employee2Service) {}
+
+    @Post()
+    create() {
+        return this.employee2Service.createEmployee2();
+    }
+}
