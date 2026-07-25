1bb46e776ad5d0208e7de14147ab580f0ecd6001
Vineet <vineetgoyat1@gmail.com>
2026-07-25 18:03:07 +0530

    continue

--- src/product1/product1.controller.ts
+++ src/product1/product1.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Product1Service } from './product1.service';
 
 @Controller('product1')
-export class Product1Controller {}
+export class Product1Controller {
+    constructor(private readonly product1Service: Product1Service) {}
+
+    @Post()
+    create(){
+        return this.product1Service.createProduct1();
+    }
+
+    @Get()
+    getAll(){
+        return this.product1Service.getAllProduct1();
+    }
+}
