dbde0768affe1be0416bcf115dfe022c1b3d911e
Vineet <vineetgoyat1@gmail.com>
2026-07-25 17:58:32 +0530

    continue

--- src/product1/product1.service.ts
+++ src/product1/product1.service.ts
@@ -7,5 +7,19 @@ import { InjectModel } from '@nestjs/mongoose';
 export class Product1Service {
     constructor(@InjectModel(Product1.name) private product1Model: Model<Product1>
     ) {}
-    async createProduct1(): Promise<Product1>
+    async createProduct1(): Promise<Product1> {
+        const product1 = new this.product1Model({
+            title: 'gaming laptop',
+            tags: [
+                {name: 'electronics'},
+                {name: 'gaming'},
+                {name: 'laptop'}
+            ]
+
+        })
+        return product1.save();
+    }
+    async getAllProduct1() : Promise<Product1[]> {
+        return this.product1Model.find();
+    }
 }
