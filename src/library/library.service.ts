d9116047d3c4f0d58d770ba6050a41735517bf4f
Vineet <vineetgoyat1@gmail.com>
2026-07-25 19:19:04 +0530

    continue

--- src/library/library.service.ts
+++ src/library/library.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import { Model } from 'mongoose';
+import { Library } from './schemas/library.schema';
 
 @Injectable()
 export class LibraryService {
     constructor(
         @InjectModel(Book.name) private readonly bookModel: Model<Book>,
+        @InjectModel(Library.name) private readonly libraryModel: Model<Library>,
     ){}
 }
