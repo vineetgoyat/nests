ac211f684553cd39f761e4aee4d81453334793fd
Vineet <vineetgoyat1@gmail.com>
2026-07-25 16:04:43 +0530

    continue

--- src/employee2/employee2.service.ts
+++ src/employee2/employee2.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Employee2 } from './schemas/employee2.schema';
+import { Model } from 'mongoose';
+import { Profile } from './schemas/profile.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class Employee2Service {}
+export class Employee2Service {
+    constructor(
+        @InjectModel(Employee2.name) private employee2Model: Model<Employee2>,
+        @InjectModel(Profile.name) private profileModel: Model<Profile>,
+    ){}
+}
