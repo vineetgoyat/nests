663de3a72db4077819bbbae006175c89f9e553b3
Vineet <vineetgoyat1@gmail.com>
2026-07-25 19:45:07 +0530

    continue

--- src/library/library.controller.ts
+++ src/library/library.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { LibraryService } from './library.service';
 
 @Controller('library')
-export class LibraryController {}
+export class LibraryController {
+    constructor(
+        private readonly libraryService: LibraryService
+    ){}
+
+    @Post()
+    createLibrary(){
+        return this.libraryService.createLibrary();
+    }
+
+    @Get()
+    getLibraries(){
+        return this.libraryService.getLibraries();
+    }
+
+
+}
