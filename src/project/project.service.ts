e7c6c359bf51362b49fc71201ce1a35ca4ef3675
Vineet <vineetgoyat1@gmail.com>
2026-07-26 17:54:08 +0530

    continue

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Project } from './schemas/project.schema';
+import { Model } from 'mongoose';
+import { Developer } from './schemas/developer.schema';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+    constructor(
+        @InjectModel(Developer.name) private developerModel: Model<Developer>,
+        @InjectModel(Project.name) private projectModel: Model<Project>
+    ) {}
+}
