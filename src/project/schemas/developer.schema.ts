cd6e08a65aacdd38265f0202452ce1053bfc4462
Vineet <vineetgoyat1@gmail.com>
2026-07-26 17:44:51 +0530

    continue

--- src/project/schemas/developer.schema.ts
+++ src/project/schemas/developer.schema.ts
@@ -7,6 +7,9 @@ export class Developer extends Document {
     'name': string;
 
     @Prop({type: [{type: Types.ObjectId, ref: 'Project'}]})
+    'projects': Types.ObjectId[];
 
 
 }
+
+export const DeveloperSchema= SchemaFactory.createForClass(Developer);
