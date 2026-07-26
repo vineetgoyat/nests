778570609307efcd7da7fe402248c49f968fde05
Vineet <vineetgoyat1@gmail.com>
2026-07-26 17:42:48 +0530

    continue

--- src/project/schemas/developer.schema.ts
+++ src/project/schemas/developer.schema.ts
@@ -1,2 +1,12 @@
 import {Schema, Prop, SchemaFactory} from '@nestjs/mongoose';
 import {Document, Types} from 'mongoose';
+
+@Schema({timestamps: true})
+export class Developer extends Document {
+    @Prop({required: true})
+    'name': string;
+
+    @Prop({type: [{type: Types.ObjectId, ref: 'Project'}]})
+
+
+}
